04235c9dcf6f7d056475e291b70ede050026847d
Ali ijaz <Se.Aliijaz@gmail.com>
2022-06-27 17:30:14 +0500

    side menu completed

--- lib/Widgets/custom_drawer.dart
+++ lib/Widgets/custom_drawer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:responsive_portfolio/Components/area_info_text.dart';
 import 'package:responsive_portfolio/Components/coding.dart';
-import 'package:responsive_portfolio/Components/knowledge_text.dart';
+import 'package:responsive_portfolio/Components/knowledges.dart';
 import 'package:responsive_portfolio/Components/skills.dart';
 import 'package:responsive_portfolio/Constants/constants.dart';
 import 'package:responsive_portfolio/Components/my_info.dart';
@@ -37,19 +38,38 @@ class SideMenu extends StatelessWidget {
                 const Skills(),
                 const SizedBox(height: defaultPadding),
                 const Coding(),
-                Column(
-                  children: [
-                    const Divider(),
-                    Text(
-                      "Knowledge",
-                      style: Theme.of(context).textTheme.subtitle2,
+                const Knowledges(),
+                const Divider(),
+                TextButton(
+                  onPressed: () {},
+                  child: FittedBox(
+                    child: Row(
+                      children: [
+                        Text("DOWNLOAD CV",
+                            style: Theme.of(context).textTheme.subtitle2),
+                        const SizedBox(
+                          width: defaultPadding / 2,
+                        ),
+                        SvgPicture.asset("assets/icon/download.svg"),
+                      ],
                     ),
-                    const KnowledgeText(text: "Flutter"),
-                    const KnowledgeText(text: "Dart"),
-                    const KnowledgeText(text: "Firebase"),
-                    const KnowledgeText(text: "C/C++"),
-                    const KnowledgeText(text: "MySQL"),
-                  ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: defaultPadding),
+                  color: const Color(0xff24242E),
+                  child: FittedBox(
+                    child: Row(
+                      children: [
+                        const Spacer(),
+                        SvgPicture.asset("assets/icons/linkedin.svg"),
+                        SvgPicture.asset("assets/icons/github.svg"),
+                        SvgPicture.asset("assets/icons/twitter.svg"),
+                        SvgPicture.asset("assets/icons/dribble.svg"),
+                        const Spacer(),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
